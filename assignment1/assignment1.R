ee4edd5a14b7fa7166e9714be0f47bc76285039b
NeilKingdom <neil@neilkingdom.xyz>
2024-12-30 19:47:19 -0500

    Big cleanup

--- assignment1/assignment1.R
+++ assignment1/assignment1.R
@@ -35,8 +35,8 @@ hp.gtHundred <- myCarsList[(myCarsList$Horsepower > 100),]
 acc.ltFifteen <- myCarsList[(myCarsList$Acceleration < 15),]
 
 # Output dataframes as CSV
-write.csv(hp.gtHundred, paste(getwd(),"Horsepower.csv", sep = "/"))
-write.csv(acc.ltFifteen, paste(getwd(),"Acceleration.csv", sep = "/"))
+write.csv(hp.gtHundred, paste(getwd(),"horsepower.csv", sep = "/"))
+write.csv(acc.ltFifteen, paste(getwd(),"acceleration.csv", sep = "/"))
 
 # Plot Histogram
 # This will ignore zero values in MPG column
@@ -72,17 +72,17 @@ res
 # Notes: This program assumes 32-bit IEEE-754 floating bit precision
 # Since there are 23 bits for value, the epsilon is 2^23 = 8388608
 # The assumption is that the user respects this range, and entering
-# a decimal value > epsilon will result in undefined behaviour 
+# a decimal value > epsilon will result in undefined behaviour
 
 # Convert left half of decimal to binary
 leftDecToBin <- function(dec) {
-  
+
   rem <- 0
   bin <- c()
-  
+
   while(dec != 0) {
     rem <- dec %% 2
-    if(rem > 0) 
+    if(rem > 0)
       bin[length(bin) + 1] <- 1
     else
       bin[length(bin) + 1] <- 0
@@ -93,13 +93,13 @@ leftDecToBin <- function(dec) {
 
 # Convert right half of decimal to binary
 rightDecToBin <- function(dec) {
-  
+
   rem <- 0
   bin <- c()
-  
+
   while(dec != 0) {
     rem <- (dec * 2) %% 2
-    if((rem - (dec * 2) %% 1) > 0) 
+    if((rem - (dec * 2) %% 1) > 0)
       bin[length(bin) + 1] <- 1
     else
       bin[length(bin) + 1] <- 0
@@ -114,10 +114,10 @@ main <- function() {
   input <- as.numeric(readline())
   leftHalf <- floor(abs(input))
   rightHalf <- abs(input - leftHalf)
-  
+
   leftHalf <- leftDecToBin(leftHalf)
   rightHalf <- rightDecToBin(rightHalf)
-  
+
   # Calculate how many decimal places need to be moved
   delim <- -1
   bin <- c(leftHalf, delim, rightHalf)
@@ -127,27 +127,27 @@ main <- function() {
   # Fix left shift error if leftHalf is 0
   if(!is.null(leftHalf))
     posMoved <- posMoved - 1
-  
+
   # Sign bit
   sign <- 0
   if(input < 0)
     sign <- 1
-  
+
   message(print("Sign (1-bit)", quote = FALSE))
   message(print(sign))
-  
+
   # Exponent
   exp <- leftDecToBin(127 + posMoved)
   # Fix left shift error if leftHalf is 0
   if(is.null(leftHalf))
     exp <- c(0, exp)
-  
+
   message(print("Exponent (8-bits)", quote = FALSE))
   message(print(exp))
-  
+
   # Remove delimiter
   bin <- bin[!bin %in% delim]
-  
+
   # Mantissa
   mantissa <- c()
   for(i in indexOfFirst:23)
@@ -156,11 +156,11 @@ main <- function() {
       mantissa[length(mantissa) + 1] <- bin[i+1]
     else
       mantissa[length(mantissa) + 1] <- bin[i]
-  
+
   mantissa[is.na(mantissa)] <- 0
   message(print("Mantissa (23-bits)", quote = FALSE))
   message(print(mantissa))
-} 
+}
 
 main()
 
